3bbf5da84982d6a7601ddf9e8e6902915d4575f2
Nathan Freeman <NathanLFreeman@gmail.com>
2022-07-17 12:32:40 -0400

    Added ternaries to display single project

--- src/components/projects.js
+++ src/components/projects.js
@@ -2,12 +2,19 @@ import {useState, useEffect} from 'react'
 import axios from 'axios'
 const Projects = () => {
   const [projects, setProjects] = useState([])
+  const [showProject, setShowProject] = useState(false)
+  const [showID, setShowID] = useState(null)
 
   const getProjects = () => {
     axios.get('http://localhost:8000/api/projects')
     .then( (response) => {
       setProjects(response.data)
     })
+  }
+
+  const displayProject = (projectID) => {
+    setShowID(projectID)
+    setShowProject(true)
   }
 
   useEffect(() => {
@@ -18,26 +25,25 @@ const Projects = () => {
     <section id="projects">
       Projects go here:
       {projects.map(proj => {
-        return(
-          <div className="project-card" key={proj.id}>
-            <img src={proj.img} />
+        return !showProject ? (
+          <div className="project-card" key={proj.id} onClick={() => displayProject(proj.id)}>
+            <img src={proj.img} alt={`a screen capture of ${proj.name}`}/>
             <h2>{proj.name}</h2>
             <p>{proj.blurb}</p>
-            {/* Add Links when map is working */}
-
+            <button><a target="_blank" href={proj.liveLink}>Live Link</a></button>
+            <button><a target="_blank" href={proj.githubLink}>The Code</a></button>
           </div>
-        )
+        ) : showID === proj.id ? (
+          <div className="project-display" key={"display_" + proj.id}>
+            <h1>{proj.name}</h1>
+            <p>{proj.blurb}</p>
+            <button><a target="_blank" href={proj.liveLink}>Live Link</a></button>
+            <button><a target="_blank" href={proj.githubLink}>The Code</a></button>
+          </div>
+        ) : null
       })}
     </section>
   )
 }
 
 export default Projects
-
-// (
-//   <div key={proj.id} className='project-card'>
-//     <img src={proj.img} alt={`a screen capture of ${proj.name}`} />
-//     <h2 className="project-name">{proj.name}</h2>
-
-//   </div>
-// )
