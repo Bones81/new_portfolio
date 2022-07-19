93e2ced1fbe27319021d3a5f98852aa6ddb7b046
Nathan Freeman <NathanLFreeman@gmail.com>
2022-07-18 21:18:01 -0400

    Setup Font Awesome with Test Icon

--- src/components/links.js
+++ src/components/links.js
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBirthdayCake } from "@fortawesome/free-solid-svg-icons"
+
 const Links = () => {
   return (
     <section id="links">
@@ -6,8 +9,17 @@ const Links = () => {
         <li className="link-li"><a href="https://github.com/Bones81">Github</a></li>
         <li className="link-li"><a href="https://www.instagram.com/nate.is.free/">Instagram</a></li>
       </ul>
+
+      <div className="icon-testing">
+        <i className="fa-solid fa-user"></i>
+        <i className="fa-solid fa-linkedin"></i>
+        <i className="fa-solid fa-instagram"></i>
+        <FontAwesomeIcon icon={faBirthdayCake}/>
+
+      </div>
     </section>
   )
 }
 
 export default Links
+
