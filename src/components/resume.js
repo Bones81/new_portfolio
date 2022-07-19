02fafdc138f769865ab67ef4dcea98d86717e340
Nathan Freeman <NathanLFreeman@gmail.com>
2022-07-18 23:03:23 -0400

    Add tech skills section and begin adding icons

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,9 +1,31 @@
+import { faAngular } from "@fortawesome/free-brands-svg-icons"
+import { faReact } from "@fortawesome/free-brands-svg-icons"
+import { faCss3 } from "@fortawesome/free-brands-svg-icons"
+import { faHtml5 } from "@fortawesome/free-brands-svg-icons"
+import { faJs } from "@fortawesome/free-brands-svg-icons"
+import { faNodeJs } from "@fortawesome/free-brands-svg-icons"
+import { faPython } from "@fortawesome/free-brands-svg-icons"
+import { faGit } from "@fortawesome/free-brands-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+
 const Resume = () => {
   return (
     <section id="resume">
-      Web Resume here
       <div>
         <h3>Resume Highlights</h3>
+        <h4>Technical Skills</h4>
+        <div className="tech-skills">
+          <FontAwesomeIcon icon={faHtml5} />
+          <FontAwesomeIcon icon={faCss3} />
+          <FontAwesomeIcon icon={faJs} />
+          <FontAwesomeIcon icon={faNodeJs} />
+          <FontAwesomeIcon icon={faReact} />
+          <FontAwesomeIcon icon={faAngular} />
+          <FontAwesomeIcon icon={faPython} />
+          <FontAwesomeIcon icon={faGit} />
+        </div>
+
         <details>
           <summary>Technical Experience</summary>
           <h3>General Assembly</h3>
