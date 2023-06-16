cd90954f11bbdb372a8bd0eaf4a48cb2b240d59e
Nathan Freeman <nathanlfreeman@gmail.com>
2023-06-16 19:21:48 -0400

    fix typo

--- src/components/about.js
+++ src/components/about.js
@@ -36,7 +36,7 @@ const About = () => {
       <article id="bio-text">
         <p>I am a full-stack software engineer known for my diligence, adaptability, and focus on quality. I bring discipline, craftsmanship, and creativity honed over 2 decades as a professional performer and a wealth of diverse work experiences from which to offer unique perspectives.</p>
         <p>I primarily build web applications with JavaScript/TypeScript or Python, though I will happily consider opportunites to extend myself into new tech spaces and other programming languages. I enjoy the constant learning required of a strong developer.</p>
-        <p>I have a Bachelor of Arts degree from Princeton University, and a certificate in Software Engineering from General Assembly.</p>
+        <p>I have a Bachelor of Arts degree from Princeton University and a certificate in Software Engineering from General Assembly.</p>
         <p>General Assembly also hired me as an Instructional Associate, validating my growing developer skills and taking advantage of my natural willingness to help others to learn and solve problems.</p>
         <p>I hope to achieve meaningful impact through my code, whether that means making your life or work easier, more productive, or more fun. Let's build the world you want to see, together.</p>
       </article>
